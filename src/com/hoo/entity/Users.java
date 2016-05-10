3ebebe1cb318b9525b568bb30924cc1a41489cce
zoul <85305948@qq.com>
2016-05-10 15:29:55 +0800

    commit

---
+++ src/com/hoo/entity/Users.java
@@ -0,0 +1,36 @@
+package com.hoo.entity;
+
+import java.util.HashMap;
+import java.util.List;
+import javax.xml.bind.annotation.XmlRootElement;
+ 
+/**
+ * <b>function:</b> Users Entity
+ * @author hoojo
+ * @createDate 2011-3-18 ÉÏÎç09:27:31
+ * @file Users.java
+ * @package com.hoo.entity
+ * @project CXFWebService
+ * @blog http://blog.csdn.net/IBM_hoojo
+ * @email hoojo_@126.com
+ * @version 1.0
+ */
+@XmlRootElement(name = "UserInfos")
+public class Users {
+    private List<User> users;
+    
+    public List<User> getUsers() {
+		return users;
+	}
+
+	public void setUsers(List<User> users) {
+		this.users = users;
+	}
+
+	private User[] userArr;
+    
+    private HashMap<String, User> maps;
+    
+    
+   // getter/setter
+}
